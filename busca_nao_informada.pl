3fd27cdb1d8b56be59a26d584b0ccd49444f4655
brunoric <brunoric@gmail.com>
2014-05-06 16:20:12 -0300

    Alterando referências de busca em largura para busca em profundidade. Iniciando a implementação de uma tentativa de cenário com geração automatizada de cenários.

--- busca_nao_informada.pl
+++ busca_nao_informada.pl
@@ -7,14 +7,14 @@
 % Busca não informada: usando o algoritmo de busca em profundidade.
 
 % aumentando tamanho máximo da pilha para 2GB
-:- set_prolog_stack(global, limit(2 000 000 000)).
-:- set_prolog_stack(trail,  limit(2 000 000 000)).
-:- set_prolog_stack(local,  limit(2 000 000 000)).
+:- set_prolog_stack(global,limit(2 000 000 000)).
+:- set_prolog_stack(trail, limit(2 000 000 000)).
+:- set_prolog_stack(local, limit(2 000 000 000)).
 
 % Cálculo do valor acumulado de pesos
-cumulativeWeight([], 0).
-cumulativeWeight([Head|Tail], TotalSum) :-
-    cumulativeWeight(Tail, Sum),
+cumulativeWeight([],0).
+cumulativeWeight([Head|Tail],TotalSum) :-
+    cumulativeWeight(Tail,Sum),
     TotalSum is Head + Sum.
 % ------------
 
@@ -22,7 +22,7 @@ cumulativeWeight([Head|Tail], TotalSum) :-
 generateContainers(Num,Range,Set) :-
 	Num = 1,
 	randset(1,Range,S),
-	append(S,[],Set), !.
+	append(S,[],Set),!.
 generateContainers(N1,Range,Set) :-
 	N2 is N1 - 1,
 	generateContainers(N2,Range,S1),
@@ -30,32 +30,56 @@ generateContainers(N1,Range,Set) :-
 	append(S1,S2,Set).
 % ------------
 
-% Gerador automatizado de cenários.
-% Busca em largura cenário 1: 2 containers de peso 5 e 4, peso máximo
+% Busca em profundidade cenário 1: 2 containers de peso 5 e 4,peso máximo
 % suportado pelas plataformas 10.
-depthFirst(Head,[Head]) :-
-	final(Head).
-depthFirst(Head, [Head|Tail]) :-
-	s(Head, H1),
-	depthFirst(H1, Tail).
-s(Head, H1) :-
-	edge(Head, H1).
+test1(P) :-
+	depthFirst1([[],[]],P).
+depthFirst1(Head,[Head]) :-
+	final1(Head).
+depthFirst1(Head,[Head|Tail]) :-
+	step1(Head,H1),
+	depthFirst1(H1,Tail).
+step1(Head,H1) :-
+	edgeTest1(Head,H1).
 
 % colocando o primeiro container
-edge([[],[]], [[5],[]]).
-edge([[],[]], [[],[5]]).
-edge([[],[]], [[4],[]]).
-edge([[],[]], [[],[4]]).
+edgeTest1([[],[]],[[5],[]]).
+edgeTest1([[],[]],[[],[5]]).
+edgeTest1([[],[]],[[4],[]]).
+edgeTest1([[],[]],[[],[4]]).
 
 % colocando o segundo container
-edge([[5],[]], [[5],[4]]).
-edge([[5],[]], [[5,4],[]]).
-edge([[],[5]], [[4],[5]]).
-edge([[],[5]], [[],[5,4]]).
-edge([[4],[]], [[4],[5]]).
-edge([[4],[]], [[4,5],[]]).
-edge([[],[4]], [[5],[4]]).
-edge([[],[4]], [[],[4,5]]).
+edgeTest1([[5],[]],[[5],[4]]).
+edgeTest1([[5],[]],[[5,4],[]]).
+edgeTest1([[],[5]],[[4],[5]]).
+edgeTest1([[],[5]],[[],[5,4]]).
+edgeTest1([[4],[]],[[4],[5]]).
+edgeTest1([[4],[]],[[4,5],[]]).
+edgeTest1([[],[4]],[[5],[4]]).
+edgeTest1([[],[4]],[[],[4,5]]).
 
-%final([[5,4],[]]).
+final([[5,4],[]]).
 final([[],[5,4]]).
+% ------------
+
+% Busca em profundidade cenário 2: 2 containers de pesos aleatórios,peso máximo
+% suportado pelas plataformas 10.
+test2(P) :-
+	depthFirst2([[],[]],P).
+depthFirst2(Head,[Head]) :-
+	final2(Head).
+depthFirst2(Head,[Head|Tail]) :-
+	step2(Head,H1),
+	depthFirst2(H1,Tail).
+step2(Head,H1) :-
+	edgeTest2(Head,H1).
+
+edgeTest2([[],[]]).
+edgeTest2(A,B) :-
+	generateContainers(2,4,Containers),
+	Containers = [Head|Tail],
+	edgeTest2([Head],Tail).
+
+final2([P]) :-
+	cumulativeWeight(P,S1),
+	S1 =< 10.
