1efa4cb520014012c45e1d3e40ff6eff637a16f0
Marjori Pomarole <marjoripomarole@gmail.com>
2014-05-05 21:23:43 -0300

    NUSP Marjori

--- busca_heuristica.pl
+++ busca_heuristica.pl
@@ -1,7 +1,7 @@
 % Grupo: 
 % Bruno Ricardo Siqueira 4953672
 % Lucas Macedo De Lemos 6920122
-% Marjori Pomarole NUSP
+% Marjori Pomarole 7152013
 % Rosaldo Alves 6309280
 %
 % Busca heurística: usando o algoritmo best fit.
